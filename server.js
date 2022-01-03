053ea593e76bcc84b9a98941810b6ccdeb699135
emeka <maduekwepedro@gmail.com>
2022-01-03 15:30:35 +0100

    fixing prod err

--- server.js
+++ server.js
@@ -12,13 +12,18 @@ const io = (module.exports.io = require("socket.io")(server));
 const socketManager = require("./handlers/socketManager");
 
 app.use(require("cors")());
-app.options("*", require("cors")());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(require("cookie-parser")());
 app.use(require("body-parser").urlencoded({ extended: true }));
 app.use(require("body-parser").json({ limit: "50mb" }));
 app.use(require("express-session")({ secret, resave: true, saveUninitialized: true }));
-
-// app.use(cors());
 
 app.use(passport.initialize());
 app.use(passport.session());
