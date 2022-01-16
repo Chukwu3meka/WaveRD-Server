7e4b2428b1421197756abfdeae38e239a77ea490
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-01-16 19:26:24 +0100

    fixing task

--- library/weeklyTask/index.js
+++ library/weeklyTask/index.js
@@ -3,12 +3,12 @@
 const weeklyTask = () => {
   try {
     // task to run every friday
-    // if (new Date().getDay() === 5) {
-    require("./emotion")();
-    require("./formation")();
-    require("./maxBudget")();
-    require("./clubReview")();
-    // }
+    if (new Date().getDay() === 5) {
+      require("./emotion")();
+      require("./formation")();
+      require("./maxBudget")();
+      require("./clubReview")();
+    }
 
     console.log("*********************** _ TASK HAS COMPLETED _ *********************");
     return "*********************** _ TASK HAS COMPLETED _ *********************";
