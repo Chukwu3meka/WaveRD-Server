056bd7be15c1e3fc0dd64d44cbb2c953147c0dca
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-01-30 00:39:01 +0100

    increased daily fitness boost from 5 to 10

--- library/dailyTask/fitness.js
+++ library/dailyTask/fitness.js
@@ -3,7 +3,7 @@ const { massList } = require("../../source/constants");
 
 module.exports = async () => {
   for (const mass of massList) {
-    await Player(mass).updateMany({ energy: { $lt: 95 } }, { $inc: { energy: 5 } });
+    await Player(mass).updateMany({ energy: { $lt: 90 } }, { $inc: { energy: 10 } });
 
     // reduce days left to recovery daily
     await Player(mass).updateMany(
