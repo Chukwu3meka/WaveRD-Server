fdafa0cb45979f294485e4f761fec0fc310fbfc2
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-01-29 23:35:05 +0100

    fix

--- handlers/admin.js
+++ handlers/admin.js
@@ -550,7 +550,7 @@ exports.starter = async (req, res) => {
 
     // require("../library/matchTask/cup")({ matchDate: "Wed Jan 05 2022", matchType: "cup" });
     // require("../library/matchTask");
-    // require("../library/dailyTask");
+    require("../library/dailyTask");
 
     // const dailyTask = require("../library/dailyTask");
 
