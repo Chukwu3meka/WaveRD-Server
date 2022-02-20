c6429ba0b4e4604f01250a175e97d2d41b8c8513
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-02-20 23:23:16 +0100

    testing cron job

--- utils/task/index.js
+++ utils/task/index.js
@@ -1,5 +1,5 @@
 const cron = require("node-cron");
 
 cron.schedule("10-15 * * * * *", () => {
-  console.log("running a task every second");
+  console.log("running a task every second", new Date());
 });
