6432365c65836c738fe06c1f8f9193346771bca1
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2022-12-29 23:33:35 +0100

    added handler and route for signup

--- source/routes/auth.ts
+++ source/routes/auth.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import signin from "../handlers/auth/signin";
+import signup from "../handlers/auth/signup";
 
 const router = express.Router();
 
 router.route("/signup").post(signin);
+router.route("/signup").post(signup);
 
 export default router;
