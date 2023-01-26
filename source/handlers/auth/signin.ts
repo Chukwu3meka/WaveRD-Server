a9b65b44305fdf867d71814d94d55efbe235a5c7
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-01-26 22:04:52 +0100

    modified auth route

--- source/handlers/auth/signin.ts
+++ source/handlers/auth/signin.ts
@@ -3,7 +3,7 @@ import { catchError } from "../../utils/handlers";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
-    return res.status(200).json("signin");
+    return res.status(200).json("successfull signin");
   } catch (err) {
     return catchError({ res, err, message: "unable to locate masses" });
   }
--- source/handlers/auth/signup.ts
+++ source/handlers/auth/signup.ts
@@ -3,7 +3,7 @@ import { catchError } from "../../utils/handlers";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
-    return res.status(200).json("signup");
+    return res.status(200).json("successfull signup");
   } catch (err) {
     return catchError({ res, err, message: "unable to locate masses" });
   }
