23a92e843ba3f02da1bf5b9b884d1a18886d272e
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-01-30 20:29:14 +0100

    added db connections

--- source/libs/mongoose.ts
+++ source/libs/mongoose.ts
@@ -24,15 +24,33 @@ import mongoose from "mongoose";
 
 // export default { config };
 
-mongoose.set("debug", true);
-mongoose.set("strictQuery", false);
-mongoose.Promise = global.Promise;
+// mongoose.set("debug", true);
+// mongoose.set("strictQuery", false);
+// mongoose.Promise = global.Promise;
 
-const v1MongoDB = mongoose.createConnection(process.env.V1_MONGODB_URI as string).;
-const apiMongoDB = mongoose.createConnection(process.env.API_MONGODB_URI as string);
-const gameMongoDB = mongoose.createConnection(process.env.GAME_MONGODB_URI as string);
+// const connectionEvents=()=>{
 
-export { v1MongoDB, apiMongoDB, gameMongoDB, mongoose as default };
+// }
+
+const v1MongoDB = mongoose
+  .createConnection(process.env.V1_MONGODB_URI as string)
+  .on("connected", () => console.log("MongoDB V1 Database connected"))
+  .on("error", () => console.log("MongoDB V1 Database encountered error"))
+  .on("disconnected", () => console.log("MongoDB V1 Database disconnected"));
+
+const hubMongoDB = mongoose
+  .createConnection(process.env.API_MONGODB_URI as string)
+  .on("connected", () => console.log("MongoDB Hub Database connected"))
+  .on("error", () => console.log("MongoDB Hub Database encountered error"))
+  .on("disconnected", () => console.log("MongoDB Hub Database disconnected"));
+
+const gameMongoDB = mongoose
+  .createConnection(process.env.GAME_MONGODB_URI as string)
+  .on("connected", () => console.log("MongoDB Game Database connected"))
+  .on("error", () => console.log("MongoDB Game Database encountered error"))
+  .on("disconnected", () => console.log("MongoDB Game Database disconnected"));
+
+export { v1MongoDB, hubMongoDB, gameMongoDB, mongoose as default };
 
 // // getting-started.js
 // const mongoose = require("mongoose");
