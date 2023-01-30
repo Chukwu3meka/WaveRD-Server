aab2fc50c8051cfa3360b1d7ebbf75b44c717ec5
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-01-30 22:23:02 +0100

    added db model

--- source/schema/index.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-const v1MongoDB = mongoose.createConnection(process.env.V1_MONGODB_URI as string);
-const apiMongoDB = mongoose.createConnection(process.env.API_MONGODB_URI as string);
-
-export default mongoose;
