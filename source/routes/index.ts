2f1fe2e76e1ce871e16096993205142e17f70732
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-01-31 12:47:42 +0100

    fixed server rediret to client

--- source/routes/index.ts
+++ source/routes/index.ts
@@ -1,23 +1,20 @@
 import v1Auth from "./v1/auth";
 
-import redirectToWeb from "./redirectToWeb";
-// import express, { Request, Response } from "express";
+import { Request, Response } from "express";
+const redirectToWeb = (req: Request, res: Response) => res.writeHead(302, { Location: process.env.CLIENT_BASE_URL }).end();
 
 export default (app: any) => {
   // ? Redirect Calls
-  app.use("/", redirectToWeb); // domain
-  app.use("/app-api", redirectToWeb); // v1.domain
-  app.use("/hub-api", redirectToWeb); // v1.domain
-  app.use("/game-api", redirectToWeb); // api.domain
+  app.all("/", redirectToWeb); // Server Homepage
+  app.all("/app-api", redirectToWeb); // app Homepage
+  app.all("/hub-api").redirectToWeb; // hub Homepage
+  app.all("/game-api").redirectToWeb; // game Homepage
 
-  // ? Internal Calls
+  // ? App-API Request
   app.use("/app-api/auth", v1Auth);
 
-  // app.use("/v1/auth/signin", (req: Request, res: Response) => {
-  //   console.log(req.headers, req.method);
-  //   return res.status(200).json("v1 successfull signin");
-  // });
-
-  // ? External Calls
-  // ....
+  // ? Hub-API Request
+  // ? Game-API Request
 };
+
+//
