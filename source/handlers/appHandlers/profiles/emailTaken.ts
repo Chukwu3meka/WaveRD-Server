11beaa82cbec734d2c0ff6bea8ccd898652eff47
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-02 23:41:02 +0100

    separeted concern in profiles/emailtaken for reusability

--- source/handlers/appHandlers/profiles/emailTaken.ts
+++ source/handlers/appHandlers/profiles/emailTaken.ts
@@ -1,20 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import { appModels } from "../../../models";
 
-// import ProfileModel from "../../../model/app_schema/profile";
-import { catchError, requestHasBody, sleep } from "../../../utils/handlers";
+import { catchError, requestHasBody } from "../../../utils/handlers";
 import validator from "../../../utils/validator";
+
+const emailExistsFn = async (email: string) => {
+  validator({ type: "email", value: email });
+
+  const dbResponse = await appModels.ProfileModel.findOne({ email });
+
+  return !!dbResponse;
+};
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     requestHasBody({ body: req.body, required: ["email"] });
     const { email } = req.body;
 
-    validator({ type: "email", value: email });
+    const emailExists = await emailExistsFn(email);
 
-    const dbResponse = await appModels.ProfileModel.findOne({ email });
-
-    const data = { success: true, message: null, payload: { emailTaken: !!dbResponse } };
+    const data = { success: true, message: null, payload: { emailTaken: emailExists } };
 
     res.status(200).json(data);
   } catch (err: any) {
