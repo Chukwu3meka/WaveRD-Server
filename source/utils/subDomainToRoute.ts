518908880932c1ac38046ffc87530f792f5342b2
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-06 23:44:38 +0100

    updated subDomaintoroute handler

--- source/utils/subDomainToRoute.ts
+++ source/utils/subDomainToRoute.ts
@@ -2,9 +2,10 @@ import { Request, Response, NextFunction } from "express";
 
 const subDomains = (app: any) => {
   app.all("*", function (req: Request, res: Response, next: NextFunction) {
-    if (req.headers.host == `app-api.${process.env.SERVER_DOMAIN}`) req.url = `/app-api${req.url}`; // ? <= direct call from app
-    if (req.headers.host == `hub-api.${process.env.SERVER_DOMAIN}`) req.url = `/hub-api${req.url}`; // ?  <= calls to soccer manager
-    if (req.headers.host == `game-api.${process.env.SERVER_DOMAIN}`) req.url = `/game-api${req.url}`; // ?  <= external call to apihub
+    if (req.headers.host == `hub.${process.env.SERVER_DOMAIN}`) req.url = `/hub${req.url}`; // ?  <= calls to SoccerMASS API Hub
+    if (req.headers.host == `logs.${process.env.SERVER_DOMAIN}`) req.url = `/logs${req.url}`; // ?  <= SoccerMASS Internal calls to logs
+    if (req.headers.host == `game.${process.env.SERVER_DOMAIN}`) req.url = `/game${req.url}`; // ?  <= SoccerMASS direct call from Manager
+    if (req.headers.host == `accounts.${process.env.SERVER_DOMAIN}`) req.url = `/accounts${req.url}`; // ? <= SoccerMASS Internal/External calls for accounts
 
     next();
   }); //Port is important if the url has it
