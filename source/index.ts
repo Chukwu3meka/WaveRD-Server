724bddbf5069fbc03b62ca57a6258920add9c5b7
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-07 02:19:57 +0100

    moved all middleware serving all request to single point

--- source/index.ts
+++ source/index.ts
@@ -6,11 +6,9 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
 
-import appRoutes from "./routes"; // enable app access database
-
+import appRoutes from "./routes";
 import passport from "./middleware/passport";
-import path_from_subdomain from "./middleware/allRequests";
-import request_logger from "./middleware/request_logger";
+import allRequests from "./middleware/allRequests";
 
 const server = async () => {
   try {
@@ -26,11 +24,9 @@ const server = async () => {
     // Apply the middleware to all incoming requests
     app.use(passport.initialize());
     app.use(passport.session());
-    app.use(path_from_subdomain);
-    app.use(request_logger);
+    app.use(allRequests); // <= handle all requests hitting server
 
-    // subDomainToRoute(app); // <= handle request redirect from specified sub domain
-    appRoutes(app); // app routes goes here
+    appRoutes(app); //  app routes goes here
 
     app.listen(port, () => console.log(`SoccerMASS:::listening on port ${port}`));
   } catch (error: any) {
--- source/middleware/allRequests.ts
+++ source/middleware/allRequests.ts
@@ -1,12 +1,27 @@
 import { Request, Response, NextFunction } from "express";
+import AllRequestModel from "../schema/logs/allRequests";
+import { catchError } from "../utils/handlers";
 
-const path_from_subdomain = (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.host == `hub.${process.env.SERVER_DOMAIN}`) req.url = `/api/hub${req.url.replace("/api", "")}`; // ?  <= calls to SoccerMASS API Hub
-  if (req.headers.host == `logs.${process.env.SERVER_DOMAIN}`) req.url = `/api/logs${req.url.replace("/api", "")}`; // ?  <= SoccerMASS Internal calls to logs
-  if (req.headers.host == `game.${process.env.SERVER_DOMAIN}`) req.url = `/api/game${req.url.replace("/api", "")}`; // ?  <= SoccerMASS direct call from Manager
-  if (req.headers.host == `accounts.${process.env.SERVER_DOMAIN}`) req.url = `/api/accounts${req.url.replace("/api", "")}`; // ? <= SoccerMASS Internal/External calls for accounts
+const allRequests = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // ? "Translating subdomains into valid server paths"
 
-  next(); //Port is important if the url has it
+    if (req.headers.host == `hub.${process.env.SERVER_DOMAIN}`) req.url = `/api/hub${req.url.replace("/api", "")}`; // ?  <= calls to SoccerMASS API Hub
+    if (req.headers.host == `logs.${process.env.SERVER_DOMAIN}`) req.url = `/api/logs${req.url.replace("/api", "")}`; // ?  <= SoccerMASS Internal calls to logs
+    if (req.headers.host == `game.${process.env.SERVER_DOMAIN}`) req.url = `/api/game${req.url.replace("/api", "")}`; // ?  <= SoccerMASS direct call from Manager
+    if (req.headers.host == `accounts.${process.env.SERVER_DOMAIN}`) req.url = `/api/accounts${req.url.replace("/api", "")}`; // ? <= SoccerMASS Internal/External calls for accounts
+
+    //  ? "Server request monitoring system"
+    const subdomain = req.headers.host?.split(".")[0];
+    const endpoint = req.url;
+
+    await AllRequestModel.create({ endpoint, subdomain });
+
+    next(); //Port is important if the url has it
+  } catch (err: any) {
+    if (process.env.NODE === "development") console.log(err.message);
+    return catchError({ res, err, status: err.status, message: "Request didn't pass middleware test" });
+  }
 };
 
-export default path_from_subdomain;
+export default allRequests;
