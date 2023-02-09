ed08b72c8f02e5090bb53e05fcac488107670c0f
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-09 23:27:25 +0100

    production fixx for cors

--- source/routes/accounts/index.ts
+++ source/routes/accounts/index.ts
@@ -2,8 +2,20 @@ import cors from "cors";
 
 import personal from "./personal";
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://manager.localhost:3000",
+  "http://apihub.localhost:3000",
+
+  "https://soccermass.com",
+  "https://manager.soccermass.com",
+  "https://apihub.soccermass.com",
+];
+
 const corsOptions = {
-  origin: [/(?:[a-zA-Z0-9]+\.)*localhost:3000/, /(?:[a-zA-Z0-9]+\.)*soccermass/, /(?:[a-zA-Z0-9]+\.)*10\.128\.32\.176:3000/],
+  // origin: [/(?:[a-zA-Z0-9]+\.)*localhost:3000/, /(?:[a-zA-Z0-9]+\.)*soccermass/, /(?:[a-zA-Z0-9]+\.)*10\.128\.32\.176:3000/],
+  //   origin: [/(?:[a-zA-Z0-9]+\.)*localhost:3000/, /(?:[a-zA-Z0-9]+\.)*soccermass/, /(?:[a-zA-Z0-9]+\.)*10\.128\.32\.176:3000/],
+  origin: allowedOrigins,
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
