518f9b0ce2844273c6cb514137d90491da79a449
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-21 17:39:14 +0100

    refActored logout

--- source/handlers/accounts/personal/logout.ts
+++ source/handlers/accounts/personal/logout.ts
@@ -3,12 +3,7 @@ import { NextFunction, Request, Response } from "express";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const cookie = req.cookies.SoccerMASS;
-    if (!cookie) throw { message: "User not Authenticated" };
-
-    const data = { success: true, message: `Successful Logout`, payload: { status: "Logout Successful" } };
-
-    return res.status(200).clearCookie("session").clearCookie("session.sig").clearCookie("SoccerMASS").json(data);
+    return res.status(200).clearCookie("session").clearCookie("session.sig").clearCookie("SoccerMASS").redirect(`http://${process.env.CLIENT_DOMAIN}/`);
   } catch (err: any) {
     return catchError({ res, err, status: err.status, message: err.message });
   }
