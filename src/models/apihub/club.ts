001e8805f540990c725d444ad7d10d5342ed0f4a
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-04-17 18:39:24 +0100

    Bug Fix && New Features
    Fix request error

--- src/models/apihub/club.ts
+++ src/models/apihub/club.ts
@@ -5,9 +5,9 @@ import { apihubDatabase } from "../database";
 const ClubSchema = new Schema({
   coach: { type: String, required: true },
   title: { type: String, required: true },
-  nickname: { type: String, required: true },
   founded: { type: Number, required: true },
   stadium: { type: String, required: true },
+  nickname: { type: String, required: true },
   capacity: { type: Number, required: true },
   location: { type: String, required: true },
   rivals: [{ type: String, required: true }],
