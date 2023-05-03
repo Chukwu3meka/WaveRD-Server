405c8cb0872eb93273d2cab62bfc5939993286bc
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-03 11:28:27 +0100

    update email exists handler

--- src/handlers/accounts/emailExists.ts
+++ src/handlers/accounts/emailExists.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 import validator from "../../utils/validator";
 import { PROFILE } from "../../models/accounts";
@@ -10,17 +10,17 @@ export const emailExistsFn = async (email: string) => {
   return !!dbResponse;
 };
 
-export default async (req: Request, res: Response, next: NextFunction) => {
+export default async (req: Request, res: Response) => {
   try {
     requestHasBody({ body: req.body, required: ["email"] });
+
     const { email } = req.body;
-
     const emailExists = await emailExistsFn(email);
-
     const data = { success: true, message: `${email} is ${emailExists ? "taken" : "available"}`, payload: { exists: emailExists } };
 
     res.status(200).json(data);
   } catch (err: any) {
+    err.status = 409;
     return catchError({ res, err });
   }
 };
