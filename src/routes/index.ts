5930a807d5c816b689cc74b93f02c8e3628b35f8
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-18 17:39:56 +0100

    bug fix, optimization and new features
    update contact us handler and add console to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,9 +7,11 @@ import * as corsOptions from "../utils/corsOptions";
 
 // routes
 import accounts from "./accounts";
+import console from "./console";
 
 export default (app: Application) => {
   app.use("/v1/accounts/", cors(corsOptions.accounts), accounts);
+  app.use("/v1/console/", cors(corsOptions.accounts), console);
 
   app.use("*", redirectToWeb); // ? Fallback URL redirects to client (web home page)
 };
