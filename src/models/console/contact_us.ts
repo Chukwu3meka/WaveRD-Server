5d27b7e2ed6351daa8be955036c5e65588069003
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-18 22:46:00 +0100

    bug fix, optimization and new features
    add contact preference to contact us

--- src/models/console/contact_us.ts
+++ src/models/console/contact_us.ts
@@ -3,10 +3,11 @@ import { Schema } from "mongoose";
 import { consoleDatabase } from "../database";
 
 const ContactUsSchema = new Schema({
-  date: { type: String, default: new Date().toDateString() },
-  category: { type: String, required: true },
   contact: { type: String, required: true },
   comment: { type: String, required: true },
+  category: { type: String, required: true },
+  preference: { type: String, required: true },
+  date: { type: String, default: new Date().toDateString() },
 });
 
 const DailyStatModel = consoleDatabase.model("Contact_Us", ContactUsSchema);
