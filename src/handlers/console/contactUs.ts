1693dbee4786982b58faf2dd7242ce6f1860a0d4
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-18 22:27:05 +0100

    bug fix, optimization and new features
    set contactus schema to generic contact instead of having multiple email|mobil|whatsapp etc

--- src/handlers/console/contactUs.ts
+++ src/handlers/console/contactUs.ts
@@ -6,13 +6,13 @@ import { catchError, requestHasBody } from "../../utils/handlers";
 export default async (req: Request, res: Response) => {
   try {
     requestHasBody({ body: req.body, required: ["category", "comment"] });
-    const { category, comment, email, mobile } = req.body;
+    const { category, comment, email, whatsapp } = req.body;
 
     if (!["others", "advertising", "technical", "suggestion", "service"].includes(category)) throw { message: "Invalid category specified", client: true };
 
-    if (!email && !mobile) throw { message: "Contact info not specified", client: true };
+    if (!email && !whatsapp) throw { message: "Contact info not specified", client: true };
 
-    await CONTACT_US.create({ category, comment, email, mobile });
+    await CONTACT_US.create({ category, comment, contact: email || whatsapp });
 
     const data = { success: true, message: `Notice sent successfully`, payload: null };
 
--- src/models/console/contact_us.ts
+++ src/models/console/contact_us.ts
@@ -5,9 +5,8 @@ import { consoleDatabase } from "../database";
 const ContactUsSchema = new Schema({
   date: { type: String, default: new Date().toDateString() },
   category: { type: String, required: true },
-  email: { type: String },
+  contact: { type: String, required: true },
   comment: { type: String, required: true },
-  mobile: { type: String },
 });
 
 const DailyStatModel = consoleDatabase.model("Contact_Us", ContactUsSchema);
