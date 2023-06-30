70b69ffc8b7c6520395160b10ec887f031e9c4f5
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-07-01 00:59:25 +0100

    fixes

--- src/models/apihub/endpoints.ts
+++ src/models/apihub/endpoints.ts
@@ -9,18 +9,7 @@ const EndpointSchema = new Schema({
   latency: { type: String, required: true },
   description: { type: String, required: true },
   snippet: { type: String, required: true },
-  response: [
-    {
-      ref: { type: String, required: true },
-      title: { type: String, required: true },
-      nickname: { type: String, required: true },
-      founded: { type: Number, required: true },
-      stadium: { type: String, required: true },
-      capacity: { type: Number, required: true },
-      location: { type: String, required: true },
-      manager: { type: String, required: true },
-    },
-  ],
+  response: { type: String, required: true },
 });
 
 const EndpointModel = apihubDatabase.model("Endpoints", EndpointSchema);
