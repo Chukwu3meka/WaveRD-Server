681ae94b850c1385409835c0c78316837ef34807
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2024-01-30 19:49:01 +0100

    bug fix, optimization and new features
    fix cors error

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Application } from "express";
 // import { createProxyMiddleware } from "http-proxy-middleware";
 
 // utils
-import * as corsOptions from "../utils/corsOptions";
+import corsOptions from "../utils/corsOptions";
 
 // routes
 import apihub from "./apihub";
@@ -14,9 +14,9 @@ export default (app: Application) => {
   // app.use("/api/v1/console/", cors(corsOptions.accounts), console);
   // app.use("/api/v1/accounts/", cors(corsOptions.accounts), accounts); // <= public console
 
-  app.use("/v1/apihub/", cors(corsOptions.accounts), apihub);
-  app.use("/v1/console/", cors(corsOptions.accounts), console);
-  app.use("/v1/accounts/", cors(corsOptions.accounts), accounts);
+  app.use("/v1/apihub/", cors(corsOptions), apihub);
+  app.use("/v1/console/", cors(corsOptions), console);
+  app.use("/v1/accounts/", cors(corsOptions), accounts);
 
   // app.use("*", createProxyMiddleware({ target: "http://localhost:3000", changeOrigin: true })); // ? Fallback URL redirects to client (web page)
   // app.use("*", createProxyMiddleware({ target: "localhost:3000", changeOrigin: true })); // ? Fallback URL redirects to client (web page)
