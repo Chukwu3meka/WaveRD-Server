7b4a37a00bc643c4d845ca1fa96301004593d09c
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2024-02-03 01:19:02 +0100

    bug fix, optimization and new features
    fix signout issues

--- src/handlers/accounts/signout.ts
+++ src/handlers/accounts/signout.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 
 import { catchError } from "../../utils/handlers";
+import { clientCookiesOption } from "../../utils/constants";
 
 export default async (req: Request, res: Response) => {
   try {
     // return res.clearCookie("session").clearCookie("session.sig").clearCookie("SSID").redirect(302, `${process.env.API_URL}/accounts/signin`);
     // return res.clearCookie("session").clearCookie("session.sig").clearCookie("SSID").redirect(302, `/accounts/signin`);
 
-    return res.clearCookie("session").clearCookie("session.sig").clearCookie("SSID").redirect(302, `${process.env.CLIENT_URL}/accounts/signin`);
+    // return res.clearCookie("session").clearCookie("session.sig").clearCookie("SSID").redirect(302, `${process.env.CLIENT_URL}/accounts/signin`);
+
+    return res.clearCookie("SSID", clientCookiesOption).redirect(302, `${process.env.CLIENT_URL}/accounts/signin`);
   } catch (err: any) {
     return catchError({ res, err });
   }
