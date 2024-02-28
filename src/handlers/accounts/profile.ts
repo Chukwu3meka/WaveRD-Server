7171851307cdb38aad81858c9833f10246285eb6
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2024-02-28 05:31:08 +0100

    bug fix, optimization and new features

--- src/handlers/accounts/profile.ts
+++ src/handlers/accounts/profile.ts
@@ -12,15 +12,19 @@ export default async (req: Request, res: Response) => {
     if (!tempId) throw { message: "Session ID is Invalid" };
     if (!session) throw { message: "Session ID is Invalid" };
 
+    // // const id = new ObjectId(tempId);
+
     const id = new ObjectId(tempId);
     if (!isValidObjectId(id)) throw { message: "Session ID is invalid" };
 
-    validate({ type: "comment", value: id }); // <= Validate request body before processing request
-    const profile = await PROFILE.findOne({ _id: id, ["auth.session"]: session });
+    validate({ type: "comment", value: tempId }); // <= Validate request body before processing request
+    const profile = await PROFILE.findById(id);
 
     if (!profile) throw { message: "Can't find associated profile", sendError: true };
+    if (profile.auth?.session !== session) throw { message: "Can't find associated profile", sendError: true };
     if (profile.status !== "active") throw { message: "Account not active", sendError: true };
-    await PROFILE.findByIdAndUpdate({ id }, { ["auth.inactivity"]: session });
+
+    await PROFILE.findByIdAndUpdate(id, { ["auth.inactivity"]: new Date() });
 
     const { role, name, handle, theme, avatar } = profile;
     const data = { success: true, message: `Profile details retrieved successfully`, data: { theme, role, name, handle, avatar } };
